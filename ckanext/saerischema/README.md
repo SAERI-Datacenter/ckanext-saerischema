6a6a65b566f537a1394a722a6c10c4ed90194997
SAERI <saeri@sat.dundee.ac.uk>
2018-10-25 11:45:21 +0100

    Added the ability to define drop-down menus in the metadata entry form

--- ckanext/saerischema/README.md
+++ ckanext/saerischema/README.md
@@ -1,11 +1,20 @@
 # metadata_form_blank.xlsx
 
-The definition of the metadata fields
-
-# metadata_form_fields.txt
-
-The metadata field names (do not change these once you've started populating CKAN)
+The definition of the metadata fields. This was used to create metadata_form_fields.txt and the metadata_form_options files. If you change this spreadsheet you should also change those files.
 
 # metadata_form_to_ckan.sh
 
-Script to read metadata_form_fields.txt and update the CKAN extension files (plugin.py etc) to display and store the metadata fields when a dataset is created/updated/displayed.
+Script to read metadata_form_fields.txt and metadata_form_options files and update the CKAN extension files (plugin.py etc) to display and store the metadata fields when a dataset is created/updated/displayed.
+
+# metadata_form_fields.txt
+
+The metadata field names, one per line, tab separated label and description.
+Do not change these once you've started populating CKAN otherwise the content of the database will be inconsistent.
+
+# metadata_form_options
+
+These files are used to create the drop-down menus when a new dataset is created. They are one option per line, tab separated id and label.
+
+# Note
+
+There are two fields which re-use the built-in defaults (Description and Tags) but with different names (Abstract and Keywords respectively).
--- ckanext/saerischema/metadata_form_to_ckan.sh
+++ ckanext/saerischema/metadata_form_to_ckan.sh
@@ -91,14 +91,53 @@ sed -i.bak '/SAERISCHEMA_ADDINFO_START/,/SAERISCHEMA_ADDINFO_END/!b;//!d;/SAERIS
 # The input file must be tab-separated Label Description
 # where the Description will be used as the placeholder
 
+# This function reads tab-separated options from a file and
+# appends <option> statements to the given filename.
+read_options_file()
+{
+	options_input_file="$1"    # read optionvalue<tab>optiontext lines from this file
+	options_output_file="$2"   # append to this file
+	options_id="$3"            # The HTML form name, eg. field-saeri_topic_category
+	options_title="$4"         # The dropdown title, eg. "Topic Category"
+	echo "Creating drop-down menu for $options_title"
+
+	echo '<div class="form-group control-medium">' >> "${options_output_file}"
+	echo '  <label for="field-'${options_id}'" class="control-label">'${options_title}'</label>' >> "${options_output_file}"
+	echo '  <div class="controls">' >> "${options_output_file}"
+	echo '    <select id="field-'${options_id}'" name="'${options_id}'" data-module="autocomplete">' >> "${options_output_file}"
+
+	cat "${options_input_file}" | while IFS="	" read label description; do
+		echo '      <option value="'${label}'">'${description}'</option>' >> "${options_output_file}"
+	done
+
+	echo '    </select>' >> "${options_output_file}"
+	echo '  </div>'      >> "${options_output_file}"
+	echo '</div>'        >> "${options_output_file}"
+}
+
 echo "Updating package_basic_fields.html"
 cat $file_input | while IFS="	" read label description; do
 	# Convert the label into an identifier, no spaces, lowercase, saeri_ prefix
 	ident=`echo $label | sed -e 's/^/saeri_/' -e 's/ /_/g' | tr '[:upper:]' '[:lower:]'`
 	#echo "$ident = $label"
 
-	echo "  {{ form.input('${ident}', label=_('${label}'), id='field-${ident}', placeholder=_('${description}'), value=data.${ident}, error=errors.${ident}, classes=['control-medium']) }}" >> ${file_basicfields_update}
-
+	# We normally use the "form.input" macro defined in ckan/templates/macros/form.html
+	# but there is no macro for dropdown menus so we use our own function defined above.
+	if [ "$label" == "Region" ]; then
+		read_options_file "metadata_form_options_region.txt" "${file_basicfields_update}" "$ident" "$label"
+	elif [ "$label" == "Responsible Party Role" ]; then
+		read_options_file "metadata_form_options_resp_party_role.txt" "${file_basicfields_update}" "$ident" "$label"
+	elif [ "$label" == "Access Limitations" ]; then
+		read_options_file "metadata_form_options_access_limitations.txt" "${file_basicfields_update}" "$ident" "$label"
+	elif [ "$label" == "Status" ]; then
+		read_options_file "metadata_form_options_status.txt" "${file_basicfields_update}" "$ident" "$label"
+	elif [ "$label" == "Topic Category" ]; then
+		read_options_file "metadata_form_options_topic_category.txt" "${file_basicfields_update}" "$ident" "$label"
+	elif [ "$label" == "Use Constraints" ]; then
+		read_options_file "metadata_form_options_use_constraints.txt" "${file_basicfields_update}" "$ident" "$label"
+	else
+		echo "  {{ form.input('${ident}', label=_('${label}'), id='field-${ident}', placeholder=_('${description}'), value=data.${ident}, error=errors.${ident}, classes=['control-medium']) }}" >> ${file_basicfields_update}
+	fi
 done
 
 sed -i.bak '/SAERISCHEMA_BASICFIELDS_START/,/SAERISCHEMA_BASICFIELDS_END/!b;//!d;/SAERISCHEMA_BASICFIELDS_START/r '${file_basicfields_update} ${file_basicfields}
