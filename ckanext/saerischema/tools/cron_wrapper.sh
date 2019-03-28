fc5a32fe31adcfb47fa05ea9199486ca7da4dca7
SAERI <saeri@gov.ms>
2019-03-28 13:00:20 +0000

    log file per program

--- ckanext/saerischema/tools/cron_wrapper.sh
+++ ckanext/saerischema/tools/cron_wrapper.sh
@@ -2,14 +2,20 @@
 # Run the program given as parameters inside the CKAN virtual environment
 # Log the output to ~/logs/cron.log
 
+# Configuration:
 venv=/usr/lib/ckan/default/bin/activate
 logdir=${HOME}/logs
-mkdir -p $logdir
-logfile=$logdir/cron.log
 
+# Initialise the log file (create a new one each time to prevent disk filling up)
+mkdir -p $logdir
+progname=`basename $1`
+logfile=$logdir/${progname}.log
 touch $logfile
 echo "`date` Running $@" > $logfile
 
+# If inside a virtualenv then just run the command
+# otherwise source the virtualenv script before running
+# Looking for:
 # VIRTUAL_ENV=/usr/lib/ckan/default
 
 if [ "$VIRTUAL_ENV" == "" ]; then
